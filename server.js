6cabff3626e9c29948966f79876cf0b021f20658
Kevin Souza <kevinxsouza@gmail.com>
2023-01-13 03:37:43 -0500

    update and delete routes done

--- server.js
+++ server.js
@@ -10,19 +10,18 @@ app.use(express.json());
 
 
 // routes
-// create
+// create a movie
 app.post('/movies', async (req, res) => {
   try {
     const { title, year, director, genre, rating, rank, comments } = req.body;
-    const newMovie = await pool.query(`INSERT INTO movies (title, year, director, genre, rating, rank, comments) VALUES ($1, $2, $3, $4, $5, $6, $7) RETURNING *`, [title, year, director, genre, rating, rank, comments]);
+    const newMovie = await pool.query("INSERT INTO movies (title, year, director, genre, rating, rank, comments) VALUES ($1, $2, $3, $4, $5, $6, $7) RETURNING *", [title, year, director, genre, rating, rank, comments]);
     res.json(newMovie.rows);
   } catch (err) {
     console.log(err.message);
   }
 })
 
-
-// show
+// show all movies
 app.get('/movies', async (req, res) => {
   try {
     const allMovies = await pool.query("SELECT * FROM movies");
@@ -33,6 +32,28 @@ app.get('/movies', async (req, res) => {
 })
 
 
+// update a movie
+app.put('/movies/:id', async (req, res) => {
+  try {
+    const { id } = req.params;
+    const { title, year, director, genre, rating, rank, comments } = req.body;
+    const updateMovie = await pool.query("UPDATE movies SET title = $1, year = $2, director = $3, genre = $4, rating = $5, rank = $6, comments = $7 WHERE id = $8", [title, year, director, genre, rating, rank, comments, id])
+    res.json("Movie was updated")
+  } catch (err) {
+    console.log(err.message);
+  }
+})
+
+// delete movie
+app.delete('/movies/:id', async (req, res) => {
+  try {
+    const { id } = req.params;
+    const deletedMovie = await pool.query('DELETE FROM movies WHERE ID = $1', [id])
+    res.json("Movie was deleted")
+  } catch (err) {
+    console.log(err.message);
+  }
+})
 
 app.listen(5000, () => {
   console.log('Listening...');
