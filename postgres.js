e5105825178dec0572d8b347a6d883e95dcf4239
Kevin Souza <kevinxsouza@gmail.com>
2023-01-17 14:20:41 -0500

    heroku pls run

--- postgres.js
+++ postgres.js
@@ -1,11 +1,18 @@
 const Pool = require('pg').Pool;
 
-const pool = new Pool({
+const dbConfig = ({
   user: "kevinsouza",
   password: "",
   host: "localhost",
   port: 5432,
   database: "final_project_api"
 });
+
+if(process.env.DATABASE_URL){
+	dbConfig.ssl = { rejectUnauthorized: false }
+	dbConfig.connectionString = process.env.DATABASE_URL
+}
+
+const pool = new Pool(dbConfig)
 
 module.exports = pool;
