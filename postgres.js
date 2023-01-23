bfbf25e9c36eb31f516ec2af5487fa45e3436875
Kevin Souza <kevinxsouza@gmail.com>
2023-01-23 02:59:13 -0500

    heroku again

--- postgres.js
+++ postgres.js
@@ -1,13 +1,19 @@
-require('dotenv').config();
 const Pool = require('pg').Pool;
 
-const isProduction = process.env.NODE_ENV === 'production';
+const dbConfig = ({
+  user: "kevinsouza",
+  password: "",
+  host: "localhost",
+  port: 5432,
+  database: "final_project_api"
+});
 
-const connectionString = `postgresql://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_DATABASE}`
+if(process.env.DATABASE_URL){
+	dbConfig.ssl = { rejectUnauthorized: false }
+	dbConfig.connectionString = process.env.DATABASE_URL
+}
 
-const pool = new Pool({
-  connectionString: isProduction ? process.env.DATABASE_URL : connectionString
-})
+const pool = new Pool(dbConfig)
 
 
 module.exports = pool
